67c3c2203defe9e6fb7f06205bb4917e6a5f2197
Mark Knutsen <mark.knutsen@cru.org>
2015-05-23 14:09:05 -0400

    Set up nginx

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,15 @@
 # Install packages
 apt-get update
 apt-get -y install npm make nginx gzip git-core python libssl-dev pkg-config build-essential tmux
+# Not available from repo: php-fpm docuwiki
 
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 dpkg -i node_latest_armhf.deb
+
+# Set up nginx
+mkdir -p /var/www/vhosts/pi-collab/httpdocs
+cp pi-collab.conf /etc/nginx/conf.d/
+cp user_agent_block /etc/nginx/
 
 # Create etherpad user
 useradd -m etherpad
